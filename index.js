ff2451bc25954ea9baed01f715b3ed060951030c
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-14 14:43:07 +0300

    getting_started_with_nodejs

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('This is NodeJS Tutorial for Beginners');
+console.log('This is the first tutorial on NodeJS');
+console.log('We will cover NodeJS with command line');
+// process.exit(1);
+// process.exitCode = 1;
