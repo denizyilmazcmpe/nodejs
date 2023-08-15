cc1e200c0f7dcae8ac146acb87818126f331aeea
denizyilmazcmpe <denizyilmazcmpe@gmail.com>
2023-08-15 09:26:28 +0300

    nodejs_package_manager_built_ins

---
+++ car.js
@@ -0,0 +1,14 @@
+exports.ford = {
+    branch : "Ford",
+    model : "Fiesta",
+};
+
+exports.tesla = {
+    branch : "Tesla",
+    model : "Model 3",
+};
+
+
+
+// module.exports = ford;
+// exports.data = {ford,tesla};
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-console.log('This is NodeJS Tutorial for Beginners');
-console.log('This is the first tutorial on NodeJS');
-console.log('We will cover NodeJS with command line');
+// console.log('This is NodeJS Tutorial for Beginners');
+// console.log('This is the first tutorial on NodeJS');
+// console.log('We will cover NodeJS with command line');
 // process.exit(1);
 // process.exitCode = 1;
+
+// const _ = require("lodash");
+// const arr = [1, 4, 6, 8];
+// console.log(_.last(arr));
+
+const { tesla,ford } = require("./car");
+console.log(JSON.stringify(tesla, null, 3));
+console.log(JSON.stringify(ford, undefined, 2));
